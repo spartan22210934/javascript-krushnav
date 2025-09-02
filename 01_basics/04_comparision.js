6eaf50944834a4c4d56a343367363b9c1f3069f4
spartan22210934 <parbatkrushnav04@gmail.com>
2025-09-02 14:48:12 +0530

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+console.log('Hello, World!');   
+
+
+
+console.log("2">1);
+console.log("02">1);
+//ts doesnt allow two diff types of comparison
+console.log(null > 0);
+console.log(null === 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined === 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+//strict mode in JavaScript === 
