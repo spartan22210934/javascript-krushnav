153781a6c4e1479b2aa586b1d03a6a3f8aa598c6
spartan22210934 <parbatkrushnav04@gmail.com>
2025-09-02 14:32:33 +0530

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+// let someNumber = 10;
+// let stringNumber = String(someNumber);
+// console.log(typeof someNumber); // number
+// console.log(typeof stringNumber); // string
+
+
+
+// let str1 = "Hello ";
+// let str2 = "krushnav!";
+// let str3 = str1 + str2;
+// console.log(str3); // Hello krushnav!
+// console.log("1"+2+2);
+// console.log(1+"2"); // 3
+// console.log(2+2+"5") ; // string
+// console.log(+true);
+
